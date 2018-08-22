dde89af1e3cac6a6c2d665ade57a374703c1efc2
aleics <aleics.casanovas@gmail.com>
2018-08-22 17:30:52 +0200

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# rustql-web
+Web application for [rustql](https://github.com/aleics/rustql) using the [React](https://reactjs.org/) framework.
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <p>Oh, hi Mark!</p>
+    );
+  }
+}
+
+export default App;
